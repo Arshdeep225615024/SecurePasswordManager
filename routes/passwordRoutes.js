38e7c7174b6405965e685fb709f1dccae09d4a75
Arshdeep225615024 <s225615024@deakin.edu.au>
2025-09-29 16:04:37 +1000

    update passwordRoutes.js

--- routes/passwordRoutes.js
+++ routes/passwordRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { savePassword, getPasswords, deletePassword } = require("../controllers/passwordController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/save-password", savePassword);
-router.get("/passwords", getPasswords);
-router.delete("/passwords/:id", deletePassword);
+router.post("/save-password", authMiddleware, savePassword);
+router.get("/passwords", authMiddleware, getPasswords);
+router.delete("/passwords/:id", authMiddleware, deletePassword);
 
 module.exports = router;
